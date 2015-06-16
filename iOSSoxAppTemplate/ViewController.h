e4d6bbe52b20f78621afdd924e811ae0d9433e7d
takuro <takuro@ht.sfc.keio.ac.jp>
2015-06-16 17:05:00 +0900

    Initial Commit

---
+++ iOSSoxAppTemplate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSSoxAppTemplate
+//
+//  Created by Takuro Yonezawa on 6/16/15.
+//  Copyright (c) 2015 Takuro Yonezawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOSSoxAppTemplate/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOSSoxAppTemplate
+//
+//  Created by Takuro Yonezawa on 6/16/15.
+//  Copyright (c) 2015 Takuro Yonezawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
